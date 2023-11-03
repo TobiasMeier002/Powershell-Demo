d7cbfb9a287edcc58ad9ebc151b2e5fb0aeed4e1
TobiasMeier002 <tobias.meier@stud.hslu.ch>
2023-11-03 16:59:24 +0100

    Update Remoting

--- remoting.ps1
+++ remoting.ps1
@@ -6,6 +6,8 @@ Get-PSSession
 
 Invoke-Command -Session $Session -ScriptBlock {[System.Environment]::OSVersion.Version}
 
+Invoke-Command -Session $Session -ScriptBlock {lsb_release -a}
+
 Invoke-Command -Session $Session -ScriptBlock {get-Process | Select-Object -First 5}
 
 Enter-PSSession -Session $Session
